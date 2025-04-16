7220490b3692d30e7200e55c22861b121267355e
micher12 <michelasm3@gmail.com>
2025-04-16 12:17:20 -0300

    updade: train 16/04/25

--- classify.py
+++ classify.py
@@ -28,7 +28,7 @@ def pegar_noticias():
     dados = []
 
     while True:
-        news = api.news_api(q="meio ambiente", country="br", language="pt", page=page)
+        news = api.news_api(category="environment", country="br", language="pt", page=page)
 
         artigos = news['results']
 
